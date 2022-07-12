fe731ae1aa3f9bdfde5dcc6bba23bec6bde982a2
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-07-12 09:11:53 +0100

    ExpressJs with NodeJs

---
+++ 02-Express.js/06-filtering-paths/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>The Page requested is not available</h1>');
+});
+
+app.listen(5500);
+
